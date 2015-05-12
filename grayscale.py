4074d6154d2036ab9e5fba9fb2b358432024ce83
Praseetha-KR <voidimagineer@yahoo.com>
2015-05-12 23:21:04 +0530

    Save generated grayscale image

--- grayscale.py
+++ grayscale.py
@@ -4,5 +4,6 @@ import cv2
 img = cv2.imread('media/img.jpg', cv2.WINDOW_NORMAL)
 cv2.imshow('image', img)
 cv2.waitKey(5000)
+cv2.imwrite('media/img_gray_gen.png', img)
 cv2.destroyAllWindows()
 cv2.waitKey(1)
