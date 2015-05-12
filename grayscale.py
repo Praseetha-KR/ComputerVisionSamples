00c7e3da472605b4bebb8834d449caa34b14fb56
Praseetha-KR <voidimagineer@yahoo.com>
2015-05-12 23:31:19 +0530

    Close image window on press ESC, save on press 's'

--- grayscale.py
+++ grayscale.py
@@ -3,7 +3,9 @@ import cv2
 
 img = cv2.imread('media/img.jpg', cv2.WINDOW_NORMAL)
 cv2.imshow('image', img)
-cv2.waitKey(5000)
-cv2.imwrite('media/img_gray_gen.png', img)
-cv2.destroyAllWindows()
-cv2.waitKey(1)
+k = cv2.waitKey(0)
+if k == 27:             # ESC key to exit
+    cv2.destroyAllWindows()
+elif k == ord('s'):     # 's' key to save & exit
+    cv2.imwrite('media/gen_img_gray.png', img)
+    cv2.destroyAllWindows()
