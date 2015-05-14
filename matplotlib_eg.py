4bcab4de6369003283910f54acb425d2dae921a5
Praseetha-KR <voidimagineer@yahoo.com>
2015-05-14 21:49:43 +0530

    use of matplotlib to convert to grayscale

---
+++ matplotlib_eg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+img = cv2.imread('media/img.jpg', 0)
+plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+plt.xticks([]), plt.yticks([])
+plt.show()
